8f848d3a231bfc081a4ff12f3ef5c9afe58c4723
Yuhuang Hu <duguyue100@gmail.com>
2017-09-12 14:01:27 +0200

    update

--- setup/user-setup.sh
+++ setup/user-setup.sh
@@ -3,6 +3,8 @@
 # Environment and parameters
 USER_DIR=$(cd $(dirname $0); pwd)
 RES_DIR=$USER_DIR/nsc_res
+INSTALL_OPT="yes"
+ENABLE_INSTALL=false
 
 ENABLE_GPU=true
 
@@ -18,6 +20,27 @@ ENABLE_DMLC=false
 ENABLE_CAFFE=false
 ENABLE_CAFFE2=false
 ENABLE_TORCH=false
+
+# turn on all options if true
+if [ $ENABLE_DL_ALL = true ]; then
+    ENABLE_TENSORFLOW=true
+    ENABLE_THEANO=true
+    ENABLE_PYTORCH=true
+    ENABLE_KERAS=true
+    ENABLE_DMLC=true
+    ENABLE_CAFFE=true
+    ENABLE_CAFFE2=true
+    ENABLE_TORCH=true
+fi
+
+# turn off some options if there is no python support
+if [ $ENABLE_PYTHON = false]; then
+    ENABLE_TENSORFLOW=false
+    ENABLE_THEANO=false
+    ENABLE_PYTORCH=false
+    ENABLE_KERAS=false
+    ENABLE_DMLC=false
+fi
 
 # get anaconda link
 if [ $ENABLE_PYTHON = true]; then
@@ -51,6 +74,50 @@ fi
 
 # Functions
 
+print_config()
+{
+    echo "[MESSAGE] Install Configuration:"
+    echo "[MESSAGE] Working directory: $USER_DIR"
+    echo "[MESSAGE] Resources directory: $RES_DIR"
+    if [ $ENABLE_PYTHON = true ]; then
+        echo "[MESSAGE] Python Enabled. Version: $PYTHON_VERSION"
+    else
+        echo "[MESSAGE] Python Disabled."
+    fi
+    if [ $ENABLE_GPU = true ]; then
+        echo "[MESSAGE] GPU Setup."
+    else
+        echo "[MESSAGE] CPU Setup."
+    fi
+    echo "[MESSAGE] Deep Learning Libraries to install:"
+    echo "[MESSAGE] TensorFlow: $ENABLE_TENSORFLOW"
+    echo "[MESSAGE] Theano: $ENABLE_THEANO"
+    echo "[MESSAGE] PyTorch: $ENABLE_PYTORCH"
+    echo "[MESSAGE] DMLC (including xgboost, mxnet): $ENABLE_DMLC"
+    echo "[MESSAGE] Caffe: $ENABLE_CAFFE"
+    echo "[MESSAGE] Caffe2: $ENABLE_CAFFE2"
+    echo "[MESSAGE] Torch: $ENABLE_TORCH"
+
+    # waiting for installation
+    echo "[MESSAGE] Do you confirm the installation options? (yes/no) [$INSTALL_OPT]"
+    read input
+    if [[ $input == "" ]]; then
+        input=$INSTALL_OPT
+    fi
+    
+    is_yes()
+    {
+        yesses={y,Y,yes,Yes,YES}
+        if [[ $yesses =~ $1 ]]; then
+            echo 1
+        fi
+    }
+
+    if [[ $(is_yes $input) ]]; then
+        $ENABLE_INSTALL=true 
+    fi
+}
+
 setup_en()
 {
     echo "[MESSAGE] Setting up installation environment"
@@ -80,82 +147,75 @@ clean_env()
 
 config_dl()
 {
-    # turn on all options if true
-    if [ $ENABLE_DL_ALL = true ]; then
-        ENABLE_TENSORFLOW=true
-        ENABLE_THEANO=true
-        ENABLE_PYTORCH=true
-        ENABLE_KERAS=true
-        ENABLE_DMLC=true
-        ENABLE_CAFFE=true
-        ENABLE_CAFFE2=true
-        ENABLE_TORCH=true
-    fi
-
-    # turn off some options if there is no python support
-    if [ $ENABLE_PYTHON = false]; then
-        ENABLE_TENSORFLOW=false
-        ENABLE_THEANO=false
-        ENABLE_PYTORCH=false
-        ENABLE_KERAS=false
-        ENABLE_DMLC=false
-    fi
-
     # common setup for python 2
     if [ $ENABLE_PYTHON = true ]; then
+        echo "[MESSAGE] Installing common packages for Python..."
         if [ $PYTHON_VERSION = 2 ]; then
             conda install -y h5py pydot graphviz
         elif [ $PYTHON_VERSION = 3]; then
             conda install -y h5py
         fi
+        echo "[MESSAGE] Common packages for Python Installed..."
     fi
 
     # tensorflow
     if [ $ENABLE_TENSORFLOW = true ]; then
-        # install tensorflow
+        echo "[MESSAGE] Installing TensorFlow..." 
         pip install -U $TENSORFLOW_URL
+        echo "[MESSAGE] TensorFlow Installed."
     fi
 
     # theano
     if [ $ENABLE_THEANO = true ]; then
+        echo "[MESSAGE] Installing Theano..."
         conda install -y theano
+        echo "[MESSAGE] Theano Installed"
     fi
 
     # pytorch
     if [ $ENABLE_PYTORCH = true ]; then
+        echo "[MESSAGE] Installing PyTorch..."
         if [ $ENABLE_GPU = true ]; then
             conda install -y pytorch torchvision cuda80 -c soumith
         elif [ $ENABLE_GPU = false ]; then
             conda install -y pytorch torchvision -c soumith
         fi
+        echo "[MESSAGE] PyTorch installed."
     fi
 
     # Keras
     if [ $ENABLE_KERAS = true ]; then
+        echo "[MESSAGE] Installing Keras..."
         pip install -U Keras
+        echo "[MESSAGE] Keras Installed."
     fi
 
     # DMLC
 
-    # Caffe
+    # Caffe (no python)
 
-    # Caffe2
+    # Caffe2 (no python)
     
     # Torch
 }
 
 # Workflow for setting up
+print_config
 
-# setting up resource folder
-setup_en
+if [ $ENABLE_INSTALL = true ]; then
+    # setting up resource folder
+    setup_en
 
-# setting up anaconda
-if [ $ENABLE_PYTHON = true ]; then
-    setup_anaconda
+    # setting up anaconda
+    if [ $ENABLE_PYTHON = true ]; then
+        setup_anaconda
+    fi
+
+    # setting up environment for deep learning
+    config_dl
+
+    # cleaning environment
+    clean_env
+else
+    echo "[MESSAGE] Installation interrupted."
 fi
-
-# setting up environment for deep learning
-config_dl
-
-# cleaning environment
-clean_env
